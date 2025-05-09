4ed6d304bdd51b7f6af65fb9ebf73001ad9ae4a3
Kriskata Andonov <kriskata.andonov@gmail.com>
2025-05-09 18:18:33 +0300

    Add basic routing structure and placeholder pages

---
+++ src/pages/TasksPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TasksPage() {
+    return (
+        <div>
+            <h1>Tasks Page</h1>
+            <p>Here your tasks will appear</p>
+        </div>
+    );
+}
+export default TasksPage;
