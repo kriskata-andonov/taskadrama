7969645d9b2e3294bc57b08ca3ae0d727800bf6b
Kriskata Andonov <kriskata.andonov@gmail.com>
2025-05-11 01:22:48 +0300

    Integrate AuthProvider and implement protected route for Tasks page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, BrowserRouter, Navigate } from 'react-router-dom'
 
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
@@ -9,21 +9,35 @@ import './App.css'
 
 import Navbar from './components/Navbar'
 
+import { AuthProvider, useAuth } from './context/AuthContext'
+
+function ProtectedRoute({ element }) {
+  const { currentUser } = useAuth()
+
+  if (!currentUser) {
+    return <Navigate to="/login" replace />
+  }
+
+  return element
+}
+
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        <div className="App">
-          <Navbar />
+        <AuthProvider>
+          <div className="App">
+            <Navbar />
 
-          <Routes>
-            <Route path="/" element={<TasksPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path='/tasks' element={<TasksPage />} />
-          </Routes>
-        </div>
+            <Routes>
+              <Route path="/" element={<ProtectedRoute element={<TasksPage />} />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path='/tasks' element={<ProtectedRoute element={<TasksPage />} />} />
+            </Routes>
+          </div>
+        </AuthProvider>
       </BrowserRouter>
     </>
   )
