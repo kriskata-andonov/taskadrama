64d7af9fa736d59f8897a2b7dff501177b93fc49
Kriskata Andonov <kriskata.andonov@gmail.com>
2025-05-13 22:36:15 +0300

    Add interaction UI (checkbox, delete button) to TaskItem component

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import './TaskItem.css';
 
-function TaskItem({ task }) {
+function TaskItem({ task, onToggleComplete, onDeleteTask }) {
 
+    const itemClassName = `task-item ${task.completed ? 'completed' : ''}`
+
+    const handleToggle = () => {
+        if (onToggleComplete) 
+            onToggleComplete(task.id, !task.completed)
+    }
+
+    const handleDelete = () => {
+        if (onDeleteTask) onDeleteTask(task.id)
+    }
 
     return (
-        <li className={`task-item ${task.completed ? 'completed' : ''}`}>
+        <li className={itemClassName}>
+            <input 
+            type="checkbox"
+            checked={task.completed}
+            onChange={handleToggle}
+            className="task-checkbox"
+        />
             <span className="task-description">{task.description}</span>
+            <button 
+                onClick={handleDelete}
+                className="delete-button"
+            >
+                Delete
+            </button>
         </li>
     )
 }
